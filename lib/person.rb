058836b8ac153eb7694d776ab04e610bd173ee04
John Carter <johncarter352@gmail.com>
2020-04-08 04:50:04 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+ 
+  def name=(persons_name)
+    @name = persons_name
+  end
+  
+  def name
+    @name
+  end
+  
+  def job=(new_job)
+    @job = new_job
+  end
+  
+  def job
+    @job
+  end
+end
