db446babec2568fe99416ae50a00b0a3745a64a4
Vladimir J <vladdaimpaler@protonmail.com>
2020-04-08 06:30:13 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+#  def initialize(personName)
+#    @name = personName
+#  end
+
+  def name=(personName)
+    @name = personName
+  end
+  def name
+    @name
+  end
+  
+  def job=(occupation)
+    @job = occupation
+  end
+  def job
+    @job
+  end
+end
