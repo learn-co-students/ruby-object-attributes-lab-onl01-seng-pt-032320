1c22aef657c580df465f67da1ba29960c6c85d69
Jeanette Rosario <jeanetterosario88@gmail.com>
2020-04-11 14:36:48 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+  def name= (person_name)
+    @name=person_name
+  end
+  
+  def name
+    @name
+  end
+  
+  def job= (jobname)
+    @job=jobname
+  end
+  
+  def job
+    @job
+  end
+  
+end
