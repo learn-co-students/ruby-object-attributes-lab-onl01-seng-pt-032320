71b34c5d87ff60ba47a257dd5a33e2321620b232
Loren Cipriano <loren25cipriano@gmail.com>
2020-04-16 20:53:32 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,20 @@
+
+
+class Person
+
+  def name=(personsname)
+    @name = personsname
+  end
+
+  def name
+    @name
+  end
+
+  def job=(jobs)
+    @job = jobs
+  end
+
+  def job
+    @job
+  end
+end
